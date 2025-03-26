6c2f9adf7582120af60c6d23562d5afb42d55e50
Gupta <Deepak.Gupta@analog.com>
2025-03-26 14:06:58 +0530

    added admin panel code to show user and task model data

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User, Task
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'mobile')
+    search_fields = ('name', 'email')
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'status', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('status',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Task, TaskAdmin)
